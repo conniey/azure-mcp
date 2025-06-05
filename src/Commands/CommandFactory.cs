9fabdfcf60c0dbdcfb94cb4fdb28b1a09d60ccfb
Connie Yau <conniey@microsoft.com>
2025-06-05 04:09:13 -0700

    Fix commandfactory

--- src/Commands/CommandFactory.cs
+++ src/Commands/CommandFactory.cs
@@ -305,14 +305,14 @@ public class CommandFactory
         var keys = new CommandGroup("key", "Key Vault key operations - Commands for managing and accessing keys in Azure Key Vault.");
         keyVault.AddSubGroup(keys);
 
-        var secrets = new CommandGroup("secret", "Key Vault secret operations - Commands for managing and accessing secrets in Azure Key Vault.");
-        keyVault.AddSubGroup(secrets);
+        var secret = new CommandGroup("secret", "Key Vault secret operations - Commands for managing and accessing secrets in Azure Key Vault.");
+        keyVault.AddSubGroup(secret);
 
         keys.AddCommand("list", new KeyVault.Key.KeyListCommand(GetLogger<KeyVault.Key.KeyListCommand>()));
         keys.AddCommand("get", new KeyVault.Key.KeyGetCommand(GetLogger<KeyVault.Key.KeyGetCommand>()));
         keys.AddCommand("create", new KeyVault.Key.KeyCreateCommand(GetLogger<KeyVault.Key.KeyCreateCommand>()));
 
-        secrets.AddCommand("get", new KeyVault.Secret.SecretGetCommand(GetLogger<KeyVault.Secret.SecretGetCommand>(),
+        secret.AddCommand("get", new KeyVault.Secret.SecretGetCommand(GetLogger<KeyVault.Secret.SecretGetCommand>(),
             _serviceProvider.GetRequiredService<IKeyVaultService>()));
     }
 
